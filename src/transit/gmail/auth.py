8013919a390f8613e2236b60ad21f21860bc84b9
erik <eriktyryshkin@gmail.com>
2022-06-25 00:59:22 -0700

    refactored gmail module, added yt to mp3 convertor, and created worker pool for handling email requests

--- src/transit/gmail/auth.py
+++ src/transit/gmail/auth.py
@@ -8,8 +8,8 @@ from google_auth_oauthlib.flow import InstalledAppFlow
 class Auth:
 
     SCOPES = [
-        'https://www.googleapis.com/auth/gmail.send',
-        'https://www.googleapis.com/auth/gmail.compose',
+        "https://www.googleapis.com/auth/gmail.send",
+        "https://www.googleapis.com/auth/gmail.compose",
     ]
 
     def __init__(self, credentials_file_path, token_file_path):
@@ -20,17 +20,19 @@ class Auth:
         creds = None
         if os.path.exists(self._token_file_path):
             creds = Credentials.from_authorized_user_file(
-                self._token_file_path, self.SCOPES)
+                self._token_file_path, self.SCOPES
+            )
 
         if creds:
             if not creds.valid and creds.expired and creds.refresh_token:
                 creds.refresh(Request())
             return creds
-        
+
         flow = InstalledAppFlow.from_client_secrets_file(
-            self._credentials_file_path, scopes=self.SCOPES)
+            self._credentials_file_path, scopes=self.SCOPES
+        )
         creds = flow.run_console()
-        with open(self._token_file_path, 'w') as token:
+        with open(self._token_file_path, "w") as token:
             token.write(creds.to_json())
 
         return creds
