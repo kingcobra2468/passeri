c5af4b06b107292aae18a69ca9f10d4129a2eeb0
kingcobra2468 <eriktyryshkin@gmail.com>
2023-07-23 23:05:46 -0700

    Clean up dead code

--- src/db/ledger.py
+++ src/db/ledger.py
@@ -1,5 +1,5 @@
 from pymongo import MongoClient
-import time
+
 from db.mp3 import Mp3
 
 
@@ -9,7 +9,8 @@ class Mp3RequestLedger:
             host=mongodb_host, port=mongodb_port, uuidRepresentation='standard')
         self._db = self._client['passeri']
         self._mp3_col = self._db['mp3s']
-        #self._mp3_col.delete_many({})
+        # self._mp3_col.delete_many({})
+
     def insert(self, link, recipient=None):
         song = Mp3(link=link, recipient=recipient)
         self._mp3_col.insert_one(song.model_dump())
@@ -21,30 +22,3 @@ class Mp3RequestLedger:
             request['inserted_at'] = request['inserted_at'].isoformat()
 
         return requests
-
-"""
-from datetime import datetime, timedelta
-import pytz
-
-local_datetime = datetime.utcnow() - timedelta(minutes=11)
-
-# Convert the local datetime to UTC
-#timezone = pytz.timezone('US/Pacific')  # Replace 'Your_Local_Timezone' with your actual local timezone
-#local_datetime = timezone.localize(local_datetime)
-#utc_datetime = local_datetime.astimezone(pytz.utc)
-
-print(local_datetime)
-
-x = Mp3RequestLedger('10.0.1.10')
-y = Mp3(url='a', recipient='b')
-x.insert(url='a', recipient='b')
-r = x.get_all({
-    "recipient": "b",
-    "inserted_at": {"$gt": local_datetime}
-})
-
-print(r)
-"""
-
-x = Mp3RequestLedger('10.0.1.10')
-print(x.get_all({}))
